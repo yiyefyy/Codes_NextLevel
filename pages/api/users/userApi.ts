53c75e1792a2624a30fdf03093756fb9486ccd0b
fannyjian <fannyj@u.nus.edu>
2023-09-27 23:36:23 +0800

    provide crud api

--- pages/api/users/userApi.ts
+++ pages/api/users/userApi.ts
@@ -9,8 +9,42 @@ export interface User {
     name: string;
   }
 
+async function fetchData(api: string, requestOptions = {}): Promise<any> {
+  const response = await fetch(api, requestOptions)
+  const results = await response.json()
+  if (!response.ok) throw new Error(results.error)
+  return results.res
+}
+
+export async function createUser(user: User): Promise<User> {
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user)
+  };
+  const register_api = `${USERS_API}/register`
+  return fetchData(register_api, requestOptions)
+}
+
 export async function fetchAllUsers(): Promise<User[]> {
-  const response = await fetch(USERS_API)
-  const data = await response.json();
-  return data.res;
+  return fetchData(USERS_API);
+}
+
+export async function fetchUser(username: string): Promise<User> {
+  const fetchUserApi = `${USERS_API}/${username}`
+  return fetchData(fetchUserApi)
+}
+
+export async function deleteUser(username: string): Promise<void> {
+  const deleteUserApi = `${USERS_API}/${username}`
+  const requestOptions = {
+    method: "DELETE"
+  };
+  const response = await fetch(deleteUserApi, requestOptions)
+  if (!response.ok) {
+    const data = await response.json()
+    throw new Error(data.error)
+  }
 }
