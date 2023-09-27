637b89cbad2748ea96327e4c1fe984688fe4bb02
fannyjian <fannyj@u.nus.edu>
2023-09-27 20:18:21 +0800

    Revert "Revert "remove planetscale""
    This reverts commit 568c4c66256bb13ddfaa8cb66bdb93bb5285cd2c.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Card, Title, Text } from '@tremor/react';
-import { queryBuilder } from '../lib/planetscale';
+// import { queryBuilder } from '../lib/planetscale';
 import Search from './search';
 import UsersTable from './table';
 
@@ -11,11 +11,11 @@ export default async function IndexPage({
   searchParams: { q: string };
 }) {
   const search = searchParams.q ?? '';
-  const users = await queryBuilder
-    .selectFrom('users')
-    .select(['id', 'name', 'username', 'email'])
-    .where('name', 'like', `%${search}%`)
-    .execute();
+  // const users = await queryBuilder
+  //   .selectFrom('users')
+  //   .select(['id', 'name', 'username', 'email'])
+  //   .where('name', 'like', `%${search}%`)
+  //   .execute();
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
@@ -25,7 +25,7 @@ export default async function IndexPage({
       </Text>
       <Search />
       <Card className="mt-6">
-        <UsersTable users={users} />
+        {/* <UsersTable users={users} /> */}
       </Card>
     </main>
   );
