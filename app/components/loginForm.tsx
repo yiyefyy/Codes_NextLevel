27696aa613a6b61ecf95f15a7c26a96a5710a13e
fannyjian <fannyj@u.nus.edu>
2023-09-30 23:52:48 +0800

    update placeholder

--- app/components/loginForm.tsx
+++ app/components/loginForm.tsx
@@ -49,7 +49,7 @@ export default function LoginForm() {
           id="email"
           name="email"
           type="email"
-          placeholder="panic@thedis.co"
+          placeholder="johndoe@globalpsa.com"
           autoComplete="email"
           required
           className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
